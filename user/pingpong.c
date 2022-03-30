bf52e4732c78a4bc1d01d3775d5477adf251d33c
Azipro <532789173@qq.com>
2022-03-30 21:34:04 +0800

    fix bugs

--- user/pingpong.c
+++ user/pingpong.c
@@ -17,15 +17,18 @@ int main(int argc, char *argv[]){
     int pid = fork();
     if(pid == 0){
         char ping[512];
+        close(sw[0]);
+        close(fw[1]);
         write(sw[1], "pong\n", 5);
         read(fw[0], ping, 5);
         printf("%d: received %s", getpid(), ping);
     }else{
         wait((int*) 0);
         char pong[512];
+        close(fw[0]); 
+        close(sw[1]);
         read(sw[0], pong, 5);
         printf("%d: received %s", getpid(), pong);
     }
-
     exit(0);
 }
